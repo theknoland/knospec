f62724d79566a1d98a52e0755088e94980235453
DR Mindle <drmindle12358@gmail.com>
2026-06-23 16:37:28 -0400

    DNS record change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 
-const site = 'https://theknoland.github.io';
+const site = 'https://knoland.com';
 
 
 // https://astro.build/config
